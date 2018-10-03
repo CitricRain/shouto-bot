2dd2932eff7f1da0a8c0ba6483fc4b6fc129f3e3
User <user@Users-MBP.home>
2018-10-03 21:12:32 +0100

    Initial

--- commands/fight.js
+++ commands/fight.js
@@ -8,11 +8,11 @@ exports.run = (bot, message, args) => {
         Math.floor(Math.random() * 3)
         switch (hRandomNumber) {
           case 0:
-            return ('Shoto Todoroki', {files: ["./images/todoroki.png"]}).catch(console.error);
+            return 'Shoto Todoroki';
           case 1:
-            return ('Katsuki Bakugo', {files: ["./images/bakugo.png"]}).catch(console.error);
+            return 'Katsuki Bakugo';
           case 2:
-            return ('Izuku Midoriya', {files: ["./images/midoriya.png"]}).catch(console.error);
+            return 'Izuku Midoriya'
         }
     }
 
@@ -21,23 +21,23 @@ exports.run = (bot, message, args) => {
     Math.floor(Math.random() * 9)
     switch (vRandomNumber) {
       case 0:
-        return ('Overhaul', {files: ["./images/overhaul.jpg"]}).catch(console.error);
+        return 'Overhaul';
       case 1:
-        return ('Tomura Shigaraki', {files: ["./images/shigaraki.png"]}).catch(console.error);
+        return 'Shigaraki Tomura';
       case 2:
-        return ('Toga Himiko', {files: ["./images/toga.jpg"]}).catch(console.error);
+        return 'Toga Himiko';
       case 3:
-        return ('Dabi', {files: ["./images/dabi.jpg"]}).catch(console.error);
+        return 'Dabi';
       case 4:
-        return ('Magne', {files: ["./images/magne.png"]}).catch(console.error);
+        return 'Magne';
       case 5:
-        return ('Muscular', {files: ["./images/muscular.png"]}).catch(console.error);
+        return 'Muscular';
       case 6:
-        return ('Moonfish', {files: ["./images/moonfish.jpg"]}).catch(console.error);
+        return 'Moonfish';
       case 7:
-        return ('Mr Compress', {files: ["./images/mrcompress.jpg"]}).catch(console.error);
+        return 'Mr Compress';
       case 8:
-      return ('Stain', {files: ["./images/stain.jpg"]}).catch(console.error);
+      return 'Stain';
     }
 }
    getWinner
