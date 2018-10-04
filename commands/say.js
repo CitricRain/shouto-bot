54794f0131e0838cca974a4501886051572e56ff
User <user@Users-MBP.home>
2018-10-04 21:29:36 +0100

    Initial

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (bot, message, args) => {
     let shotomessage = args.join(" ");
     if(!shotomessage) return message.channel.send("You can't tell me to say nothing. Try again.")
-    if(shotomessage = 'Endeavour is a good dad') return message.channel.send("I dont lie.")
+    if(shotomessage = args.join("Endeavour is a good dad")) return message.channel.send("I dont lie.")
     message.delete().catch();
     message.channel.send(shotomessage);
 }
