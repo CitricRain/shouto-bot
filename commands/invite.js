488299f419a9e711e776e7b3cc40089d9ad151ad
User <user@Users-MBP.home>
2018-10-04 21:40:08 +0100

    Initial

--- commands/invite.js
+++ commands/invite.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => { 
     message.channel.send(`Use this link right here to invite me to your server:
-    https://discordapp.com/oauth2/authorize?&client_id=477032652793315338&scope=bot&permissions=2146958583`).catch(console.error);
+https://discordapp.com/oauth2/authorize?&client_id=477032652793315338&scope=bot&permissions=2146958583`).catch(console.error);
 }
 module.exports.help = {
     name: "invite"
