9230baf4baa1135a3f341f2de1f26f32395725f9
User <user@Users-MBP.home>
2018-10-04 21:20:51 +0100

    Initial

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
-    
+    let shotomessage = args.join(" ");
+    message.delete().catch();
+    message.channel.send(shotomessage);
 }
 module.exports.help = {
     name: "say"
