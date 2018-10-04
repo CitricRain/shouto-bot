91e14b0381cc218202011ee9efea99c702af0b1c
User <user@Users-MBP.home>
2018-10-04 21:12:29 +0100

    Initial

--- commands/clear.js
+++ commands/clear.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
-   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You can't do that, sorry.");
+   if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You can't do that, sorry.");
    if(!args[0]) return message.channel.send("I can't get rid of nothing, try again.");
    message.channel.bulkDelete(args[0]).then(() => {
        message.channel.send(`${args[0]} messages deleted.`).then(message => message.delete(5000));
