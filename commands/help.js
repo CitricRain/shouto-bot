37c23698c878732a36d002f61da289ce532e7076
User <user@Users-MBP.home>
2018-10-04 17:10:24 +0100

    Initial

--- commands/help.js
+++ commands/help.js
@@ -11,10 +11,10 @@ exports.run = (bot, message, args) => {
       .setColor("#70c168")
       .setThumbnail(hIcon)
       .addField("Basic", "`help`, `ping`, `hello`")
-      .addField("Fun", "slurp, handcrusher, selfie, punkboy, perfectboy, rowdyboy, garbageboy, fight") 
+      .addField("Fun", "`slurp`, `handcrusher`, `selfie`, `punkboy`, `perfectboy`, `rowdyboy`, `garbageboy`, `fight`") 
       .addField("Information", "`botinfo`, `serverinfo`")
       .addField("Mod Stuff", "`kick`, `ban`, `report`")
-      .addField("Character Sheets", "`Use to name of any character for a full character sheet (Currently only works for class 1-A`");
+      .addField("Character Sheets", "`Use the name of any character for a full character sheet (Currently only works for class 1-A`");
   
         return message.channel.send(helpEmbed);
    }
