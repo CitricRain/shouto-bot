078ed35746a10fd545f346dc63cc08f09a71b583
User <user@Users-MBP.home>
2018-10-04 21:23:44 +0100

    Initial

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
     let shotomessage = args.join(" ");
+    if(!shotomessage) return message.channel.send("You can't tell me to say nothing. Try again.")
     message.delete().catch();
     message.channel.send(shotomessage);
 }
