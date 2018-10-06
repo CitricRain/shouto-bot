5b66d6ee9834d73a300d0b49fbba2870b7824e2a
User <user@Users-MacBook-Pro.local>
2018-10-06 20:01:19 +0100

    Initial

--- commands/softboy.js
+++ commands/softboy.js
@@ -2,20 +2,20 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
 
-    let randomBoy = Math.floor(Math.random() * 31)
+    let randomBoy = Math.floor(Math.random() * 4)
     let person = message.author
     switch (randomBoy) {
       case 0: 
-        message.channel.send(`If you insist, ${person}.`, {files: ["./images/softboy.jpg"]}).catch(console.error);
+        message.channel.send(`A soft boy`, {files: ["./images/softboy.jpg"]}).catch(console.error);
         break;
       case 1:
-        message.channel.send(`If you insist, ${person}.`, {files: ["./images/softboy2.jpg"]}).catch(console.error);
+        message.channel.send(`A soft boy`, {files: ["./images/softboy2.jpg"]}).catch(console.error);
         break;
       case 2:
-        message.channel.send(`If you insist, ${person}.`, {files: ["./images/softboy3.jpg"]}).catch(console.error);
+        message.channel.send(`A soft boy`, {files: ["./images/softboy3.jpg"]}).catch(console.error);
         break;
       case 3:
-        message.channel.send(`If you insist, ${person}.`, {files: ["./images/softboy4.jpg"]}).catch(console.error);
+        message.channel.send(`A soft boy`, {files: ["./images/softboy4.jpg"]}).catch(console.error);
         break;  
     }
 }
