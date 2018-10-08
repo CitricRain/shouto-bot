10058bef52bff7a67c8f24f8edc35958bdf79e66
User <user@Users-MBP.home>
2018-10-08 23:16:40 +0100

    Initial

--- commands/eye.js
+++ commands/eye.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 
-exports.run = (bot, message, args) => { 
+exports.run = async (bot, message, args) => { 
     await message.channel.send("Testing");
    const msgs = await message.channel.awaitMessage(msg => {
         console.log(msg.content);
