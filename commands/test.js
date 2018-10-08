20b15e29edd2a8121b54f368868a1ea707d32c0a
User <user@Users-MBP.home>
2018-10-08 11:12:33 +0100

    Initial

--- commands/test.js
+++ commands/test.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
  
-    message.channel.send(`Is this thing on? - Took ${new Date().getTime() - message.createdTimestamp}ms`);
-    
+    message.channel.send(`:white_check_mark: Is this thing on? - Took: **${new Date().getTime() - message.createdTimestamp} ms**`);
+
     }
 
 module.exports.help = {
