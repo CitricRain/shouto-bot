984d5de894b7dc24dc4e0a45062647a009f89650
User <user@Users-MBP.home>
2018-10-08 20:27:47 +0100

    Initial

--- commands/eye.js
+++ commands/eye.js
@@ -6,7 +6,7 @@ exports.run = (bot, message, args) => {
    const msgs = await message.channel.awaitMessage(msg => {
         console.log(msg.content);
         return msg.content.includes("hi");
-    }, {time: 10000});
+    }, {time: 5000});
 
     message.channel.send(`test completed. Check console`)
     console.log(msgs);
