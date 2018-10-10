70197cb4c00d76affb945b33e7767e3af145f09c
User <user@Users-MBP.home>
2018-10-10 22:20:36 +0100

    Initial

---
+++ commands/owo.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+exports.run = (bot, message, args) => {
+    
+    let owos = ["Wats dis?", "Misew Obama?", "OwO?", "... Why am I doing this", "UwU", "*nuzzles u*", "I hwave dwaddy issuwes!", "I don't even know what this means"];
+
+    let options = Math.floor((Math.random() * replies.length));
+   
+ message.channel.send(owos[options]);
+
+};
+module.exports.help = {
+    name: "owo"
+}
