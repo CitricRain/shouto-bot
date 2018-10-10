69985ee18c32a35e94bb627547915e63a04f6d65
User <user@Users-MBP.home>
2018-10-10 17:05:34 +0100

    Intiia;

--- commands/dm.js
+++ commands/dm.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
     
-    var user = client.users.get("351480748005523466");
+    var user = users.get("351480748005523466");
     message.channel.send("Done. Check the console.")
     console.log(user);
 
