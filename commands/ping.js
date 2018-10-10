c9df9933709197db7f84165c49fbe49d25d8e5d2
User <user@Users-MBP.home>
2018-10-10 17:51:16 +0100

    initial

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
-    message.channel.send("Pong!").catch(console.error);
-}
+    let pingHelp = args.join("help").slice(22);
+    if(pingHelp) { message.channel.send("Bot resonds with 'Pong!") 
+} else message.channel.send("Pong!").catch(console.error);
+};
 module.exports.help = {
     name: "ping"
 }
