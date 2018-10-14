b12f7071e9b265ffaa9a2e011fd35ac39bf69d2f
User <user@Users-MBP.home>
2018-10-14 12:39:52 +0100

    Initial

--- commands/fight.js
+++ commands/fight.js
@@ -5,7 +5,7 @@ exports.run = (bot, message, args) => {
 
     function getHero() {
         let hRandomNumber =
-        Math.floor(Math.random() * 26)
+        Math.floor(Math.random() * 25)
         switch (hRandomNumber) {
           case 0:
             return 'Shoto Todoroki';
