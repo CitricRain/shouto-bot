e8524b54736632ebfcd71e11033e331bee8c9efb
User <user@Users-MBP.home>
2018-10-16 20:42:02 +0100

    Initial

--- commands/calc.js
+++ commands/calc.js
@@ -9,7 +9,7 @@ exports.run = async (bot, message, args) => {
     try {
         resp = math.eval(args.join(' '));
     } catch (e) {
-        return message.channel.send("Please ask a proper question.");
+        return message.channel.send("Please ask a proper question. (Tip: The symbol for divide is '/' e.g: `s!calc 4/2 = 2` and the symbol for multiply is '*' e.g: `s!calc 3*3 = 9`");
     }
     const calcEmbed = new Discord.RichEmbed()
     .setColor("#ffffff")
