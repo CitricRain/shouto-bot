7ac1f3aae50cb7fb258b34826350340a131498dc
User <user@Users-MBP.home>
2018-10-18 21:57:39 +0100

    Initial

--- commands/owo.js
+++ commands/owo.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
     
-    let owos = ["Wats dis?", "Misew Obama?", "OwO?", "... Why am I doing this", "UwU", "*nuzzles u*", "I hwave dwaddy issuwes!", "I don't even know what this means","Then Perish."];
+    let owos = ["Wats dis?", "Mistew Obama?", "OwO?", "... Why am I doing this", "UwU", "*nuzzles u*", "I hwave dwaddy issuwes!", "I don't even know what this means","Then Perish."];
 
     let options = Math.floor((Math.random() * owos.length));
    
