359bdc3d3bdbbd36a586b44c5e37e9a003e77682
User <user@Users-MBP.home>
2018-10-26 17:15:10 +0100

    Initial

--- commands/sneeze.js
+++ commands/sneeze.js
@@ -9,16 +9,16 @@ exports.run = (bot, message, args) => {
     let randomRBoy = Math.floor(Math.random() * 5)
     switch (randomRBoy) {
       case 1: 
-        message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy.jpg"]}).catch(console.error);
+        message.channel.send(`Achoo!`, {files: ["./images/sneeze.png"]}).catch(console.error);
         break;
       case 2:
-        message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy2.jpg"]}).catch(console.error);
+        message.channel.send(`I didn't think I could catch colds.`, {files: ["./images/sneeze.png"]}).catch(console.error);
         break;
       case 3:
-        message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy3.jpg"]}).catch(console.error);
+        message.channel.send(`Someone must be talking about me.`, {files: ["./images/sneeze.png"]}).catch(console.error);
         break;
       case 4:
-        message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy4.png"]}).catch(console.error);
+        message.channel.send(`Stop doing that.`, {files: ["./images/sneeze.png"]}).catch(console.error);
         break;  
     }
 }
