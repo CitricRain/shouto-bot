46e8096da25ee53233cb70dd0f94dfe6cf73979f
User <user@Users-MBP.home>
2018-11-02 16:50:07 +0000

    Initial

--- commands/rowdyboy.js
+++ commands/rowdyboy.js
@@ -6,7 +6,7 @@ exports.run = (bot, message, args) => {
 
 exports.run = (bot, message, args) => {
 
-    let randomRBoy = Math.floor(Math.random() * 5)
+    let randomRBoy = Math.floor(Math.random() * 6)
     switch (randomRBoy) {
       case 1: 
         message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy.jpg"]}).catch(console.error);
@@ -19,7 +19,10 @@ exports.run = (bot, message, args) => {
         break;
       case 4:
         message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy4.png"]}).catch(console.error);
-        break;  
+        break; 
+        case 5:
+        message.channel.send(`A rowdy boy`, {files: ["./images/rowdyboy5.jpg"]}).catch(console.error);
+        break; 
     }
 }
 }
