5a27cea9081d2b570702dcb01fc6cd01d9705a7f
User <user@Users-MBP.home>
2018-11-03 23:38:23 +0000

    Initial

--- commands/owo.js
+++ commands/owo.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
+
+    if(message.guild.id === "489941049427492864") return;
     
     let owos = ["Wats dis?", "Mistew Obama?", "OwO?", "... Why am I doing this", "UwU", "*nuzzles u*", "I hwave dwaddy issuwes!", "I don't even know what this means","Then Perish."];
 
