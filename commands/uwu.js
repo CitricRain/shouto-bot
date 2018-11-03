d892ec75fcf1d1a361c1eb8c1f84f43f2d93556d
User <user@Users-MBP.home>
2018-11-03 23:42:30 +0000

    Initial

---
+++ commands/uwu.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+exports.run = (bot, message, args) => {
+
+    if(message.guild.id === "489941049427492864") return;
+    
+    let owos = ["Wats dis?", "Mistew Obama?", "OwO?", "... Why am I doing this", "UwU", "*nuzzles u*", "I don't even know what this means","Then Perish."];
+
+    let options = Math.floor((Math.random() * owos.length));
+   
+ message.channel.send(owos[options]);
+
+};
+module.exports.help = {
+    name: "uwu"
+}
