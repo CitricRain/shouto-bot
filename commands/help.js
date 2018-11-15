cf294819f37f67d3703aae1be6a59d46a29be294
User <user@Users-MBP.home>
2018-11-15 18:03:26 +0000

    Initial

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,23 @@
  const Discord = require('discord.js');
   exports.run = (bot, message, args) => { 
   
+    if (args[0] == "test") {
+      message.reply("test complete.")
+    }
+    
+    if (!args[0]) {
       let helpEmbed = new Discord.RichEmbed()
       .setDescription("Help")
       .setColor("#70c168")
       .addField("Basic", "`help`, `ping`, `hello`")
-      .addField("Fun", "`slurp`, `handcrusher`, `selfie`, `punkboy`, `perfectboy`, `rowdyboy`, `garbageboy`, `fight`, `shotoball`, `flip`, `owo`") 
+      .addField("Fun", "`slurp`, `handcrusher`, `selfie`, `punkboy`, `perfectboy`, `rowdyboy`, `garbageboy`, `fight`, `shotoball`, `flip`, `owo`, `bonk`") 
       .addField("Information", "`botinfo`, `serverinfo`, `calc`")
       .addField("Mod Stuff", "`kick`, `ban`, `report`")
       .addField("Character Sheets", "`Use the name of any character for a full character sheet (Currently only works for class 1-A`");
   
         return message.channel.send(helpEmbed);
    } 
-
+  }
   module.exports.help = {
     name: "help"
   }
