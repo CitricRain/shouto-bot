dfaeab8cf8a01dcf65592bfcbc9f726a2329d3bf
User <user@Users-MBP.home>
2018-11-15 17:06:09 +0000

    initial

--- commands/softboy.js
+++ commands/softboy.js
@@ -2,24 +2,24 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
 
-    let randomBoy = Math.floor(Math.random() * 6)
+    let randomBoy = Math.floor(Math.random() * 7)
     switch (randomBoy) {
-      case 1: 
+      case 0: 
         message.channel.send(`A soft boy`, {files: ["./images/softboy.jpg"]}).catch(console.error);
         break;
-      case 2:
+      case 1:
         message.channel.send(`A soft boy`, {files: ["./images/softboy2.jpg"]}).catch(console.error);
         break;
-      case 3:
+      case 2:
         message.channel.send(`A soft boy`, {files: ["./images/softboy3.jpg"]}).catch(console.error);
         break;
-      case 4:
+      case 3:
         message.channel.send(`A soft boy`, {files: ["./images/softboy4.jpg"]}).catch(console.error);
         break;  
-      case 5:
+      case 4:
         message.channel.send(`A soft boy`, {files: ["./images/softboy5.jpg"]}).catch(console.error);
         break;  
-      case 6:
+      case 5:
         message.channel.send(`A soft boy`, {files: ["./images/softboy6.jpg"]}).catch(console.error);
         break;  
       case 6:
