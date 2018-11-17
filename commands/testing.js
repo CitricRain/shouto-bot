57d95ede66134eed5e0e4cf057c92c02f1962edd
User <user@Users-MBP.home>
2018-11-17 23:30:12 +0000

    Initial

--- commands/testing.js
+++ commands/testing.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 
-exports.run = async (bot, message, args) => { 
+module.exports.run = async (bot, message, args) => { 
    
     if (args[0] == "this") return message.channel.send("This works.")
     if (args[0] == "it") return message.channel.send("It works.")
