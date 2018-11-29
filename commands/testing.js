9863d2899bdd7958bbe615cc1acc0b6bef84b057
User <user@Users-MBP.home>
2018-11-29 21:46:15 +0000

    Initial

--- commands/testing.js
+++ commands/testing.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => { 
    
-    message.channel.send("This is working...").then(msg => msg.edit(5000("This is also working!")))
+    message.channel.send("This is working...").then(msg => msg.edit("This is also working!"))
 return;
 
 }
