5e9e9d39d8cf378a3599872dd58a75ce2114ae51
User <user@Users-MBP.home>
2019-01-06 15:18:45 +0000

    Initial

--- shoto.js
+++ shoto.js
@@ -55,10 +55,6 @@ bot.on("guildMemberAdd", function(member) {
   let welcome = Math.floor((Math.random() * greetings.length));
   welcomeChannel.send(greetings[welcome])
 
-  member.send(`Hello, ${member}, I am Shoto bot. I am currently a work in progress bot. If you encounter any bugs or problems, please message my creator to report them.
-Please note that since I am still a work in progress, I will be updated frequently, so be on the lookout for new features and commands.
-Other than that, please use the command **'s!help'** to take a look at what I can do.`)
-  
 });
 
 // Message event
